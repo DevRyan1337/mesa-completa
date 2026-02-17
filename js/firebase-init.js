0d5e5882d87b8882ed11b3ef476e39bb3a219efe
DevRyan1337 <ryan-morello@hotmail.com>
2026-02-17 16:25:54 -0300

    atualização firebaes

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,16 +1,15 @@
 // js/firebase-init.js
-
 const firebaseConfig = {
   apiKey: "AIzaSyAmituB9652__9Asd3kVM2VJFTT-4sCWvE",
   authDomain: "mesa-completa.firebaseapp.com",
   projectId: "mesa-completa",
-  storageBucket: "mesa-completa.firebasestorage.app",
+  storageBucket: "mesa-completa.appspot.com",
   messagingSenderId: "352211050825",
   appId: "1:352211050825:web:1e43d99d60406700c77a98"
 };
 
 firebase.initializeApp(firebaseConfig);
 
-// Deixa global para usar em qualquer página
+// Globais
 window.auth = firebase.auth();
 window.db = firebase.firestore();
